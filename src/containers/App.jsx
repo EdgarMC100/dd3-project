a81d1bf39d2e3031ef4f035de8a306818c961344
edgarmc <edgarmc100@gmail.com>
2021-11-08 12:37:07 -0600

    :bug: [ADDS] Fix of wrong import

--- src/containers/App.jsx
+++ src/containers/App.jsx
@@ -1,10 +1,9 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import '@styles/App.scss'
 import Header from './Header';
 import Main from './Main';
 import Footer from './Footer';
 import useGetData from '../hooks/useGetData';
-import { useEffect } from 'react/cjs/react.development';
 
 const App = () => {
   const [ type, setType ] = useState('buy')
