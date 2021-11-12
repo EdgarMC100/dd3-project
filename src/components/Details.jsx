3022e2a5dafec0224fb4b04eb6043a64669856c2
edgarmc <edgarmc100@gmail.com>
2021-11-12 00:56:00 -0600

    :iphone: [ADD] Fix of responsive design mobile screen minor than 786px

--- src/components/Details.jsx
+++ src/components/Details.jsx
@@ -23,7 +23,7 @@ const Details = ({houseDetail, loading}) => {
             <tr>
               <td><img src={bed} alt="bed image"/>{houseDetail.bedrooms}</td>
               <td><img src={shower} alt="shower image"/>{houseDetail.bathroom}</td>
-              <td><img src={slot} alt="slot image"/>{houseDetail.slot}</td>
+              <td><img src={slot} alt="slot image" />{houseDetail.slot}</td>
               <td><img src={garage} alt="car garage image" />{houseDetail.slot}</td>
               <td><img src={date} alt="date image" />{houseDetail.year}</td>
             </tr>
